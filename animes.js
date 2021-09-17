d50dd6d2da5cf5c56250c91a77a4438c9f15986d
caredlav <caredlav@gmail.com>
2021-09-16 19:49:49 -0500

    create an array of strings and two console.log

---
+++ animes.js
@@ -0,0 +1,4 @@
+const seriesChildhood=["Dragon Ball", "Dragon Ball Z", "Pokémon", "Digimon", "YU-GI-OH!", "Inuyasha"];
+
+console.log(seriesChildhood);
+console.log("\nSHOW ME YOUR BRAVE HEART!!!!!");
